4853249e3c585ad950970851f2c08c66754c3167
Sascha <sascha@macbooks.fritz.box>
2014-06-03 23:43:48 +0200

    Highlighted Cells get unhighlighted during the next step.

--- AlgoAnimAODV/src/generators/network/aodv/guielements/GUIController.java
+++ AlgoAnimAODV/src/generators/network/aodv/guielements/GUIController.java
@@ -11,57 +11,73 @@ import algoanim.util.Coordinates;
 public class GUIController {
 
 	private static Language lang;
-	private HashMap<AODVNode,InfoTable> tables;
+	private HashMap<AODVNode, InfoTable> tables;
 	private InfoBox info;
-	private Coordinates infoBoxUpperLeft = new Coordinates(40,470);
-	private Coordinates infoBoxLowerRight = new Coordinates(840,600);
-	private Coordinates tableStartingPont = new Coordinates(500,20);
+	private Coordinates infoBoxUpperLeft = new Coordinates(40, 470);
+	private Coordinates infoBoxLowerRight = new Coordinates(840, 600);
+	private Coordinates tableStartingPont = new Coordinates(500, 20);
 	private int distanceBetweenTables = 30;
-	
-	public GUIController(Language language){
+	private ArrayList<InfoTable> lastUpdated;
+
+	public GUIController(Language language) {
 		lang = language;
-		tables = new HashMap<AODVNode,InfoTable>();
+		tables = new HashMap<AODVNode, InfoTable>();
+		lastUpdated = new ArrayList<InfoTable>();
 	}
-	
-	
-	public void drawInfoTable(ArrayList<AODVNode> nodes){
-		
+
+	public void drawInfoTable(ArrayList<AODVNode> nodes) {
+
 		/**
 		 * Check how many tables have to be drawn in a row
 		 */
-		int numOfTablesX = (int) Math.round((nodes.size()+0.5) / 2);
-		
+		int numOfTablesX = (int) Math.round((nodes.size() + 0.5) / 2);
+
 		/**
-		 * Draw initial table in order to get the width and height for the following tables
+		 * Draw initial table in order to get the width and height for the
+		 * following tables
 		 */
-		InfoTable table = new InfoTable(lang, nodes.get(0),tableStartingPont, nodes.size());
+		InfoTable table = new InfoTable(lang, this, nodes.get(0),
+				tableStartingPont, nodes.size());
 		int offsetX = distanceBetweenTables + table.getWidth();
 		int offsetY = distanceBetweenTables + table.getHeight();
-		
-		for (int i = 1; i < nodes.size(); i++){
-			table = new InfoTable(lang, nodes.get(i),
-					(GeometryToolBox.moveCoordinate(tableStartingPont,i%numOfTablesX*offsetX,i/numOfTablesX*offsetY)), nodes.size());
-			tables.put(nodes.get(i),table);
+
+		for (int i = 1; i < nodes.size(); i++) {
+			table = new InfoTable(lang, this, nodes.get(i),
+					(GeometryToolBox.moveCoordinate(tableStartingPont, i
+							% numOfTablesX * offsetX, i / numOfTablesX
+							* offsetY)), nodes.size());
+			tables.put(nodes.get(i), table);
 		}
 
 	}
-	
-	public void highlightCell(AODVNode node, int cellX, int cellY){
+
+	public void highlightCell(AODVNode node, int cellX, int cellY) {
 		tables.get(node).highlightCell(cellX, cellY, true);
 	}
-	
-	public void unhighlightCell(AODVNode node, int cellX, int cellY){
+
+	public void unhighlightCell(AODVNode node, int cellX, int cellY) {
 		tables.get(node).highlightCell(cellX, cellY, false);
 	}
-	
-	public void drawInfoBox(String titleOfBox){
-		info = new InfoBox(lang, titleOfBox,
-				infoBoxUpperLeft, infoBoxLowerRight);
+
+	public void drawInfoBox(String titleOfBox) {
+		info = new InfoBox(lang, titleOfBox, infoBoxUpperLeft,
+				infoBoxLowerRight);
 	}
-	
-	public void updateInfoBoxText(String update){
+
+	public void updateInfoBoxText(String update) {
 		info.updateText(update);
 	}
+
+	public void tableUpdated(InfoTable table) {
+		lastUpdated.add(table);
+	}
+
+	public void tableRefresh() {
+			for (InfoTable table : lastUpdated) {
+				table.refresh();
+			}
+			lastUpdated.clear();
+		}
 	
-	
+
 }
--- AlgoAnimAODV/src/generators/network/aodv/guielements/InfoTable.java
+++ AlgoAnimAODV/src/generators/network/aodv/guielements/InfoTable.java
@@ -1,10 +1,10 @@
 package generators.network.aodv.guielements;
 
-import generators.network.aodv.AODVGraph;
 import generators.network.aodv.AODVNode;
 import generators.network.aodv.RoutingTableEntry;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.util.ArrayList;
 
 import algoanim.primitives.Rect;
@@ -16,17 +16,16 @@ import algoanim.util.Coordinates;
 
 public class InfoTable {
 
-
-	 /**
-	   * The concrete language object used for creating output
-	   */
+	/**
+	 * The concrete language object used for creating output
+	 */
 	private Language lang;
-	
+
 	/**
 	 * Height of a cell
 	 */
 	private int cellHeight = 15;
-	
+
 	/**
 	 * Width of a column
 	 */
@@ -36,27 +35,27 @@ public class InfoTable {
 	 * Color for highlighted cells
 	 */
 	private Color highlightColor = Color.ORANGE;
-	
+
 	/**
-	 * Strings for the title row 
+	 * Strings for the title row
 	 */
 	private String[] titles = new String[] { "N", "DS", "HC", "NH" };
-	
+
 	/**
 	 * Shortcut for the number of rows in the table
 	 */
 	private int numRows = titles.length;
-	
-	/** 
-	 * Shortcut for the approx. height of the whole table. 
+
+	/**
+	 * Shortcut for the approx. height of the whole table.
 	 */
 	private int height;
-	
+
 	/**
 	 * List to store the text in
 	 */
 	private ArrayList<ArrayList<Text>> textContent;
-	
+
 	/**
 	 * List to access the rectangle of each cell for highlight purposes
 	 */
@@ -66,21 +65,25 @@ public class InfoTable {
 	 * Represents the currentLine in the GUI
 	 */
 	private Coordinates currentLine;
-	
+
+	private ArrayList<Point> lastChanged;
 	
 	AODVNode ownNode;
 	int numNodes;
+	private GUIController controller;
 
-	public InfoTable(Language lang, AODVNode nodeForThisTable, Coordinates startPoint,
-			int numOfNodes) {
+	public InfoTable(Language lang, GUIController controller, AODVNode nodeForThisTable,
+			Coordinates startPoint, int numOfNodes) {
 		this.lang = lang;
+		this.controller = controller;
 		this.ownNode = nodeForThisTable;
-        this.ownNode.addTable(this);
+		this.ownNode.addTable(this);
 		this.currentLine = startPoint;
 		this.numNodes = numOfNodes;
-		height =  cellHeight * (numNodes+1);
+		height = cellHeight * (numNodes + 1);
 		this.textContent = new ArrayList<ArrayList<Text>>();
 		this.cells = new ArrayList<ArrayList<Rect>>();
+		this.lastChanged = new ArrayList<Point>();
 		initContent();
 	}
 
@@ -96,18 +99,20 @@ public class InfoTable {
 
 		nextLine();
 		for (int i = 0; i < numRows; i++) {
-			lang.newText(GeometryToolBox.moveCoordinate(currentLine, distanceColumns * i, 0),
-					titles[i], "", null);
+			lang.newText(
+					GeometryToolBox.moveCoordinate(currentLine, distanceColumns
+							* i, 0), titles[i], "", null);
 
 			if (i != 0) {
 				GeometryToolBox.drawVerticalLine(
-						GeometryToolBox.moveCoordinate(currentLine, distanceColumns * i - 5, 0),
-						height);
+						GeometryToolBox.moveCoordinate(currentLine,
+								distanceColumns * i - 5, 0), height);
 			}
 		}
 		// switch to the next line
 		nextLine();
-		GeometryToolBox.drawHorizontalLie(currentLine, distanceColumns * numRows);
+		GeometryToolBox.drawHorizontalLie(currentLine, distanceColumns
+				* numRows);
 
 		for (int i = 0; i < numNodes; i++) {
 			ArrayList<Text> textInOneLine = new ArrayList<Text>();
@@ -116,9 +121,10 @@ public class InfoTable {
 			for (int z = 0; z < numRows; z++) {
 
 				// create rectangles as cells for highlighting
-				Coordinates cellUpperLeft = GeometryToolBox.moveCoordinate(currentLine,
-						distanceColumns * z - 2, -1);
-				Coordinates cellDownRight = GeometryToolBox.moveCoordinate(currentLine,
+				Coordinates cellUpperLeft = GeometryToolBox.moveCoordinate(
+						currentLine, distanceColumns * z - 2, -1);
+				Coordinates cellDownRight = GeometryToolBox.moveCoordinate(
+						currentLine,
 						distanceColumns * z + distanceColumns - 10,
 						cellHeight - 1);
 
@@ -130,15 +136,15 @@ public class InfoTable {
 
 				// create text inside every cell
 				String textToAdd;
-				if (z == 0){
+				if (z == 0) {
 					textToAdd = "A";
 				} else {
 					textToAdd = "-";
 				}
-				
-				textInOneLine.add(lang.newText(
-						GeometryToolBox.moveCoordinate(currentLine, distanceColumns * z, 0),
-						textToAdd, "", null));
+
+				textInOneLine.add(lang.newText(GeometryToolBox.moveCoordinate(
+						currentLine, distanceColumns * z, 0), textToAdd, "",
+						null));
 			}
 			textContent.add(textInOneLine);
 			cells.add(cellsInOneLine);
@@ -148,75 +154,72 @@ public class InfoTable {
 	}
 
 	private void nextLine() {
-		currentLine = GeometryToolBox.moveCoordinate(currentLine, 0, cellHeight);
+		currentLine = GeometryToolBox
+				.moveCoordinate(currentLine, 0, cellHeight);
 	}
 
 	public void updateTable() {
+		controller.tableRefresh();
 		ArrayList<RoutingTableEntry> currentRoutingTable = ownNode
 				.getRoutingTable();
+		boolean updated = false;
 		for (int i = 0; i < textContent.size(); i++) {
 
 			if (currentRoutingTable.get(i) != null) {
 				// check node identifier
-				if (currentRoutingTable.get(i).getIdentifier() != textContent
-						.get(i).get(0).getText()) {
-					textContent
-							.get(i)
-							.get(0)
-							.setText(
-									currentRoutingTable.get(i).getIdentifier(),
-									null, null);
+				String nodeID = currentRoutingTable.get(i).getIdentifier();
+				if (!nodeID.equals(textContent.get(i).get(0).getText())) {
+					textContent.get(i).get(0).setText(nodeID, null, null);
 					highlightCell(i, 0, true);
-				} else {
-					highlightCell(i, 0, false);
-				}
+					lastChanged.add(new Point(i,0));
+					updated = true;
+					
+				} 
 
 				// check destination sequence number
-				if (Integer.toString(currentRoutingTable.get(i)
-						.getDestinationSequence()) != textContent.get(i).get(1)
-						.getText()) {
-					textContent
-							.get(i)
-							.get(1)
-							.setText(
-									Integer.toString(currentRoutingTable.get(i)
-											.getDestinationSequence()), null,
-									null);
+				String DestinationSeq = Integer.toString(currentRoutingTable
+						.get(i).getDestinationSequence());
+				if (!DestinationSeq.equals(textContent.get(i).get(1).getText())) {
+					textContent.get(i).get(1)
+							.setText(DestinationSeq, null, null);
 					highlightCell(i, 1, true);
-				} else {
-					highlightCell(i, 1, false);
-				}
+					lastChanged.add(new Point(i,1));
+					updated = true;
+					
+				} 
 
 				// check hop count
-				if (Integer.toString(currentRoutingTable.get(i).getHopCount()) != textContent
-						.get(i).get(2).getText()) {
-					textContent
-							.get(i)
-							.get(2)
-							.setText(
-									Integer.toString(currentRoutingTable.get(i)
-											.getHopCount()), null, null);
+				int hopCount = currentRoutingTable.get(i).getHopCount();
+				if (!Integer.toString(hopCount).equals(
+						textContent.get(i).get(2).getText())) {
+					String hopString;
+					if (hopCount == Integer.MAX_VALUE) {
+						hopString = "inf";
+					} else {
+						hopString = Integer.toString(hopCount);
+					}
+
+					textContent.get(i).get(2).setText(hopString, null, null);
 					highlightCell(i, 2, true);
-				} else {
-					highlightCell(i, 2, false);
-				}
+					lastChanged.add(new Point(i,2));
+					updated = true;
+					
+				} 
 
 				// check next hop
-				if (currentRoutingTable.get(i).getNextHop() != textContent
-						.get(i).get(3).getText()) {
-					textContent
-							.get(i)
-							.get(3)
-							.setText(currentRoutingTable.get(i).getNextHop(),
-									null, null);
+				String nextHop = currentRoutingTable.get(i).getNextHop();
+				if (!nextHop.equals(textContent.get(i).get(3).getText())) {
+					textContent.get(i).get(3).setText(nextHop, null, null);
 					highlightCell(i, 3, true);
-				} else {
-					highlightCell(i, 3, false);
-				}
+					lastChanged.add(new Point(i,3));
+					updated = true;
+				} 
 			}
 		}
-
-        lang.nextStep();
+		if (updated){
+		controller.tableUpdated(this);
+		}
+		lang.nextStep();
 	}
 
 	public void highlightCell(int row, int column, boolean highlight) {
@@ -224,21 +227,26 @@ public class InfoTable {
 			cells.get(row).get(column).show();
 		} else {
 			cells.get(row).get(column).hide();
-		}	
+		}
 	}
 	
-	
-	public AODVNode getAODVNode(){
-		return ownNode;
-	}
-	
-	public int getHeight(){
-		return height;
-	}
-	
-	public int getWidth(){
-		return distanceColumns*numRows;
+	public void refresh(){
+		for (Point currentPoint : lastChanged){
+			highlightCell((int)currentPoint.getX(),(int)currentPoint.getY(),false);
+		}
+		lastChanged.clear();
 	}
 
+	public AODVNode getAODVNode() {
+		return ownNode;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return distanceColumns * numRows;
+	}
 
 }
