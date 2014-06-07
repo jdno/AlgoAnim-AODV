12edb56bdf607ef179e2bd8d74e2a9b4e7feb730
Sascha <(null)>
2014-06-08 01:13:35 +0200

    Graph is now bidirectional to display arrows in both directions

--- AlgoAnimAODV/src/generators/network/aodv/guielements/GUIGraph.java
+++ AlgoAnimAODV/src/generators/network/aodv/guielements/GUIGraph.java
@@ -5,6 +5,7 @@ import algoanim.primitives.generators.Language;
 import algoanim.properties.AnimationPropertiesKeys;
 import algoanim.properties.GraphProperties;
 import algoanim.util.Node;
+import generators.graph.helpers.AdjacencyMatrix;
 import generators.network.aodv.AODVGraph;
 import generators.network.aodv.AODVNode;
 
@@ -31,7 +32,7 @@ public class GUIGraph extends GUIElement{
 
         graphProperties.set(AnimationPropertiesKeys.HIGHLIGHTCOLOR_PROPERTY, highlightColor);
         graphProperties.set(AnimationPropertiesKeys.FILL_PROPERTY, Color.WHITE);
-        graphProperties.set(AnimationPropertiesKeys.DIRECTED_PROPERTY, false);
+        graphProperties.set(AnimationPropertiesKeys.DIRECTED_PROPERTY, true);
 
         String[] graphLabels;
 
@@ -45,8 +46,21 @@ public class GUIGraph extends GUIElement{
             graphLabels[i] = loadedGraph.getNodeLabel(nodes[i]);
         }
 
-        animalGraph = lang.newGraph("Graph", loadedGraph.getAdjacencyMatrix(), nodes, graphLabels, null, graphProperties);
+        animalGraph = lang.newGraph("Graph", transformAdjacencyMatrix(loadedGraph), nodes, graphLabels, null, graphProperties);
         animalGraph.hide();
+    }
+
+    private int[][] transformAdjacencyMatrix(Graph loadedGraph){
+        int[][] adjacencyMatrix = new int[loadedGraph.getAdjacencyMatrix().length][loadedGraph.getAdjacencyMatrix()[0].length];
+        for (int i = 0; i < loadedGraph.getAdjacencyMatrix().length; i++) {
+            for (int y = 0; y < loadedGraph.getAdjacencyMatrix()[i].length;y++){
+                if (loadedGraph.getAdjacencyMatrix()[i][y] == 1){
+                    adjacencyMatrix[i][y] = 1;
+                    adjacencyMatrix[y][i] = 1;
+                }
+            }
+        }
+        return adjacencyMatrix;
     }
 
 
