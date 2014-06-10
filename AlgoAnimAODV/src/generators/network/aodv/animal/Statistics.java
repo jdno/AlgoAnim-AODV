182fdd934787cad54784668494c4b0e9244e981e
Jan David <jandavid@awesometechnology.me>
2014-06-10 22:04:49 +0200

    Add stats collector

---
+++ AlgoAnimAODV/src/generators/network/aodv/animal/Statistics.java
@@ -0,0 +1,111 @@
+package generators.network.aodv.animal;
+
+/**
+ * Class that collects various statistics about the performance
+ * of the AODV routing algorithm. This class implements the
+ * singleton pattern to act as the one collector for ALL stats.
+ *
+ * @author Jan David
+ */
+public class Statistics {
+
+    /**
+     * Shared instance of Statistics
+     */
+    private static Statistics sharedInstance = null;
+
+    /**
+     * Number of messages sent
+     */
+    private int messageCount = 0;
+
+    /**
+     * Number of routes discovered
+     */
+    private int routesDiscoveredCount = 0;
+
+    /**
+     * Number of read accesses and comparisons in the routing tables
+     */
+    private int routingTableReadsCount = 0;
+
+    /**
+     * Number of write accesses in the routing tables
+     */
+    private int routingTableUpdatesCount = 0;
+
+    /**
+     * Statistics implements the Singleton pattern. This method returns
+     * the shared instance of Statistics.
+     *
+     * @return The shared instance of Statistics
+     */
+    public static Statistics sharedInstance() {
+        if (sharedInstance == null) {
+            sharedInstance = new Statistics();
+        }
+
+        return sharedInstance;
+    }
+
+    /**
+     * Private constructor for Statistics. Access from outside this class
+     * should always be done using sharedInstance().
+     */
+    private Statistics() {
+
+    }
+
+    /**
+     * Increment the number of sent messages by one.
+     */
+    public void messageSent() {
+        messageCount++;
+    }
+
+    public void routeDiscovered() {
+        routesDiscoveredCount++;
+    }
+
+    /**
+     * Increment the number of routing table read accesses.
+     */
+    public void routingTableRead() {
+        routingTableReadsCount++;
+    }
+
+    /**
+     * Increment the number of routing table write accesses.
+     */
+    public void routingTableUpdated() {
+        routingTableUpdatesCount++;
+    }
+
+    /**
+     * @return The message count
+     */
+    public int getMessageCount() {
+        return messageCount;
+    }
+
+    /**
+     * @return The routes discovered count
+     */
+    public int getRoutesDiscoveredCount() {
+        return routesDiscoveredCount;
+    }
+
+    /**
+     * @return The routing table reads count
+     */
+    public int getRoutingTableReadsCount() {
+        return routingTableReadsCount;
+    }
+
+    /**
+     * @return The routing table update count
+     */
+    public int getRoutingTableUpdatesCount() {
+        return routingTableUpdatesCount;
+    }
+}
