d5969883109eb58ed01eced359df2beb35cbb88f
Jan David <jandavid@awesometechnology.de>
2014-07-28 11:59:27 +0200

    Finalize comments for TableCell

--- AlgoAnimAODV/src/generators/network/aodv/guielements/Tables/TableCell.java
+++ AlgoAnimAODV/src/generators/network/aodv/guielements/Tables/TableCell.java
@@ -8,13 +8,39 @@ import algoanim.util.Coordinates;
 import generators.network.aodv.guielements.GUIElement;
 import generators.network.aodv.guielements.GeometryToolBox;
 
+/**
+ * This class represents a cell in a GUITable.
+ *
+ * @author Sascha Bleidner, Jan David Nose
+ */
 public class TableCell extends GUIElement {
 
+    /**
+     * The content of the cell
+     */
 	private Text entry;
+
+    /**
+     * The rectangle that gets highlighted. It is placed below the text, and gets
+     * hidden when no hightlight is necessary.
+     */
 	private final Rect highlightBox;
+
+    /**
+     * The look & feel of the hightlight box
+     */
     private static RectProperties boxProperties;
 
-	
+    /**
+     * Create a new TableCell.
+     *
+     * @param lang The language object
+     * @param text The content of the cell
+     * @param position The position of the cell
+     * @param width The width of the cell
+     * @param height The height of the cell
+     * @param rectProps The look & feel of the highlight
+     */
 	public TableCell(Language lang, String text, Coordinates position, int width, int height, RectProperties rectProps){
 		super(lang,position);
 
@@ -26,24 +52,40 @@ public class TableCell extends GUIElement {
 		this.highlightBox.hide();
 		this.entry = lang.newText(position, text, "cell", null);
 	}
-	
+
+    /**
+     * Highlight the cell
+     */
 	public void highlightCell(){
 		highlightBox.show();
 	}
-	
+
+    /**
+     * Reset the highlight of the cell
+     */
 	public void unhighlightCell(){
 		highlightBox.hide();
 	}
-	
+
+    /**
+     * @return the text
+     */
 	public String getText(){
 		return entry.getText();
 	}
-	
+
+    /**
+     * @param text the text
+     */
 	public void setText(String text){
         entry.hide();
         entry = lang.newText(position, text,"cell",null);
 	}
 
+    /**
+     * Update the text only if it has changed
+     * @param text the text
+     */
     public void updateText(String text){
         if (!text.equals(entry)){
             setText(text);
